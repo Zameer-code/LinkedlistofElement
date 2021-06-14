8cdd3041b36683d7b49096fac1f9993008fb11df
Zameer-code <84001846+Zameer-code@users.noreply.github.com>
2021-06-14 11:10:26 +0530

    Deleting  the first element in the Linked list

--- src/main/java/com/linkedlist/Runner.java
+++ src/main/java/com/linkedlist/Runner.java
@@ -6,10 +6,14 @@ public class Runner {
 	public static void main(String[] args) {
 				
 		LinkedList<Integer> numberList = new LinkedList<>();
-			
-		numberList.addAtStart(56);
-		numberList.add(1, 30);
+		 numberList.add(56);
+		numberList.add(30);
 		numberList.add(70);
+		System.out.println("before deleting");
+		numberList.show();
+		System.out.println();
+		System.out.println("After deleting");
+		numberList.popFirst();
 		numberList.show();
 
 	    }
