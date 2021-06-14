819c7ec81517c93ea468539bfe6b916c9838d122
Zameer-code <84001846+Zameer-code@users.noreply.github.com>
2021-06-14 11:22:23 +0530

    insert 40 after 30 to the Linked List sequence of 56->30->70

--- src/main/java/com/linkedlist/Runner.java
+++ src/main/java/com/linkedlist/Runner.java
@@ -5,14 +5,14 @@ public class Runner {
 	
 	public static void main(String[] args) {
 				
-		LinkedList<Integer> numberList = new LinkedList<>();
 		 numberList.add(56);
-	         numberList.add(30);
-	         numberList.add(70);
+		 numberList.add(30);
+		 numberList.add(70);
 		 numberList.show();
-	         System.out.println();
-	         int result = numberList.search(30);
-	         System.out.println("index  " + result);
+		 System.out.println();
+		 int result = numberList.search(30);
+		 numberList.add(result + 1, 40);
+		 numberList.show();
 
 	        
 	    }
