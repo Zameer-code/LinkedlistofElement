85d69a322e2f290fe7ea1d9ca7f7edbcacc16bb3
Zameer-code <84001846+Zameer-code@users.noreply.github.com>
2021-06-14 11:13:03 +0530

    Update Runner.java

--- src/main/java/com/linkedlist/Runner.java
+++ src/main/java/com/linkedlist/Runner.java
@@ -7,14 +7,14 @@ public class Runner {
 				
 		LinkedList<Integer> numberList = new LinkedList<>();
 		 numberList.add(56);
-		numberList.add(30);
-		numberList.add(70);
-		System.out.println("before deleting");
-		numberList.show();
-		System.out.println();
-		System.out.println("After deleting");
-		numberList.popFirst();
-		numberList.show();
+		 numberList.add(30);
+		 numberList.add(70);
+		 System.out.println("before deleting");
+		 numberList.show();
+		 System.out.println();
+		 System.out.println("After deleting");
+		 numberList.getFirst();
+		 numberList.show();
 
 	    }
 }
