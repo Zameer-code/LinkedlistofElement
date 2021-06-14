b87282967554de0aa619494d2ce0bac88f738aad
Zameer-code <84001846+Zameer-code@users.noreply.github.com>
2021-06-14 11:28:38 +0530

    Update Runner.java

--- src/main/java/com/linkedlist/Runner.java
+++ src/main/java/com/linkedlist/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
 			
 	
 	public static void main(String[] args) {
-				
+		 LinkedList<Integer> numberList = new LinkedList<>();
 		 numberList.add(56);
 		 numberList.add(30);
 		 numberList.add(70);
