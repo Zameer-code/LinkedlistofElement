71cf4eff8904cfe72494190f0f2f518ae850f3ef
Zameer-code <84001846+Zameer-code@users.noreply.github.com>
2021-06-14 11:25:48 +0530

    Update Runner.java

--- src/main/java/com/linkedlist/Runner.java
+++ src/main/java/com/linkedlist/Runner.java
@@ -4,19 +4,19 @@ public class Runner {
 			
 	
 	public static void main(String[] args) {
-				
-		numberList.add(56);
-	        numberList.add(30);
-	        numberList.add(40);
-	        numberList.add(70);
-	        numberList.show();
-	        System.out.println();
-	        System.out.println("post deletion..");
-	        int result = numberList.search(40);
-	        numberList.remove(result);
-	        int Size = numberList.size();
-	        numberList.show();
-	        System.out.println("Size of list " + Size);
+		LinkedList<Integer> numberList = new LinkedList<>();	
+			numberList.add(56);
+			numberList.add(30);
+			numberList.add(40);
+			numberList.add(70);
+			numberList.show();
+			System.out.println();
+			System.out.println("post deletion..");
+			int result = numberList.search(40);
+			numberList.remove(result);
+			int Size = numberList.size();
+			numberList.show();
+			System.out.println("Size of list " + Size);
 	        
 	    }
 }
