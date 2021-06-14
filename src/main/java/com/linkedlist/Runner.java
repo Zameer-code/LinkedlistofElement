c6f2aef69d31b3b9019f832c5253bdb1c535b164
Zameer-code <84001846+Zameer-code@users.noreply.github.com>
2021-06-14 11:24:26 +0530

    delete 40 from the Linked List sequence of 56->30->40->70 and show the size of LinkedList is 3

--- src/main/java/com/linkedlist/Runner.java
+++ src/main/java/com/linkedlist/Runner.java
@@ -5,15 +5,18 @@ public class Runner {
 	
 	public static void main(String[] args) {
 				
-		 numberList.add(56);
-		 numberList.add(30);
-		 numberList.add(70);
-		 numberList.show();
-		 System.out.println();
-		 int result = numberList.search(30);
-		 numberList.add(result + 1, 40);
-		 numberList.show();
-
+		numberList.add(56);
+	        numberList.add(30);
+	        numberList.add(40);
+	        numberList.add(70);
+	        numberList.show();
+	        System.out.println();
+	        System.out.println("post deletion..");
+	        int result = numberList.search(40);
+	        numberList.remove(result);
+	        int Size = numberList.size();
+	        numberList.show();
+	        System.out.println("Size of list " + Size);
 	        
 	    }
 }
